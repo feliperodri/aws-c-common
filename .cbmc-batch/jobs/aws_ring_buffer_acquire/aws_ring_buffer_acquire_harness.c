e2052af1d66094a3719b79943bed4e2e535355fa
Felipe R. Monteiro <felisous@amazon.com>
2019-06-10 11:55:28 -0400

    volatile problem
    Signed-off-by: Felipe R. Monteiro <felisous@amazon.com>

--- .cbmc-batch/jobs/aws_ring_buffer_acquire/aws_ring_buffer_acquire_harness.c
+++ .cbmc-batch/jobs/aws_ring_buffer_acquire/aws_ring_buffer_acquire_harness.c
@@ -18,11 +18,14 @@
 #include <proof_helpers/make_common_data_structures.h>
 #include <proof_helpers/proof_allocators.h>
 
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+
 void aws_ring_buffer_acquire_harness() {
     /* parameters */
     struct aws_ring_buffer ring_buf;
     size_t ring_buf_size;
     size_t requested_size;
+    size_t available_size;
     struct aws_byte_buf buf;
 
     /* assumptions */
@@ -31,14 +34,26 @@ void aws_ring_buffer_acquire_harness() {
     ensure_byte_buf_has_allocated_buffer_member(&buf);
     __CPROVER_assume(aws_byte_buf_is_valid(&buf));
 
+    if(ring_buf.head == ring_buf.tail) {
+        available_size = ring_buf_size;
+    } else if (ring_buf.tail > ring_buf.head) {
+        available_size = ring_buf.tail - ring_buf.head;
+    } else if (ring_buf.tail < ring_buf.head) {
+        available_size = MAX(ring_buf.allocation_end - ring_buf.head, ring_buf.tail - ring_buf.allocation);
+    }
     if (aws_ring_buffer_acquire(nondet_bool() ? &ring_buf : NULL, requested_size, nondet_bool() ? &buf : NULL) ==
         AWS_OP_SUCCESS) {
         /* assertions */
+        assert(available_size >= requested_size);
         assert(aws_ring_buffer_is_valid(&ring_buf));
         assert(aws_byte_buf_is_valid(&buf));
         assert(AWS_MEM_IS_WRITABLE(buf.buffer, requested_size));
         assert(buf.capacity == requested_size);
         assert(buf.len == 0); /* aws_byte_buf always created with aws_byte_buf_from_empty_array */
         assert(buf.buffer >= ring_buf.allocation && (buf.buffer + buf.capacity) <= (ring_buf.allocation_end));
+    } else {
+        if (aws_last_error() == AWS_ERROR_OOM) {
+            assert(available_size < requested_size);
+        }
     }
 }
